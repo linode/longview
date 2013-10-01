6ff1a347950d53882fe11fbfd525e56af1d12b4b
Peter Sandin <psandin@linode.com>
2013-10-01 08:20:28 -0400

    Client version 1.0.1

--- Linode/Longview/Util.pm
+++ Linode/Longview/Util.pm
@@ -54,7 +54,7 @@ our $logger = get_logger();
 our $gua;
 our $post_target   = 'https://longview.linode.com/post';
 
-our $VERSION = '1.0.0';
+our $VERSION = '1.0.1';
 our $TICKS   = POSIX::sysconf(&POSIX::_SC_CLK_TCK);
 our $PROCFS  = find_procfs()      or $logger->logdie("Couldn't find procfs: $!");
 our $ARCH    = get_architecture() or $logger->info("Couldn't determine architecture: $!");
